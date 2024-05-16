12f65dbdbff219db2bb06e2fdec3936ef5b40889
sjmitch-git <sjmitch20@outlook.com>
2024-05-16 17:57:24 +0100

    Adds Server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const http = require("http");
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
+    res.end("A HTTP server listening on PORT:8080");
   })
   .listen(8080);
 
